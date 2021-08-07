576a19e867f12f7edaadc8767147d61a17923985
vikash kaushik <vikashkaushik232@gmail.com>
2021-08-07 10:52:16 +0530

    day 26 completed

---
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/cart.dart';
+import 'package:flutter_application_1/models/catalog.dart';
+import 'package:flutter_catalog/models/cart.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class AddToCart extends StatelessWidget {
+  const AddToCart({
+    Key key,
+    this.catalog,
+  }) : super(key: key);
+
+  final Item catalog;
+  final _cart = CartModel();
+  @override
+  Widget build(BuildContext context) {
+    bool isInCart = _cart.items.contains(catalog) ?? false;
+    return ElevatedButton(
+      onPressed: () {
+        if (!isInCart) {
+          isInCart = isInCart.toggle();
+          final _catalog = CatalogModel();
+
+          _cart.catalog = _catalog;
+          _cart.add(catalog);
+          // setState(() {});
+        }
+      },
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(context.theme.buttonColor),
+          shape: MaterialStateProperty.all(
+            StadiumBorder(),
+          )),
+      child: isInCart ? Icon(Icons.done) : Icon(CupertinoIcons.cart_badge_plus),
+    );
+  }
+}
